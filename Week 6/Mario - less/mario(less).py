3a0e0b5785f34b555f64f5003319f7916835d171
satyakinkohli <59170604+satyakinkohli@users.noreply.github.com>
2020-08-08 20:50:06 +0530

    Create mario(less).py

---
+++ Week 6/Mario - less/mario(less).py
@@ -0,0 +1,19 @@
+height = input("What is the height of your pyramid?: ")
+
+while (height.isdigit() is False or height is None):
+    print("\nPlease enter a value between 1 and 9 (both inclusive).")
+    height = (input("What is the height of your pyramid?: "))
+
+height = (int)(height)
+while (height < 1 or height > 8):
+    print("\nPlease enter a value between 1 and 9 (both inclusive).")
+    height = int(input("What is the height of your pyramid?: "))
+
+for i in range(1, height + 1, 1):
+    for j in range(height, i, -1):
+        print(" ", end="")
+
+    for k in range(0, i, 1):
+        print("#", end="")
+
+    print()
